a98c7534a830b412bdff4e6c7125e2ccf6a7edd3
Neki <84998222+Nekidev@users.noreply.github.com>
2022-12-02 18:38:12 -0300

    Removed .next folder

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/libraries/python":["static\u002Fchunks\u002Fpages\u002Flibraries\u002Fpython.js"],sortedPages:["\u002F_app","\u002F_error","\u002Flibraries\u002Fpython"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
