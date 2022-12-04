9ac0ec8a0d6007c7447f89da05c8471186f2cd82
Neki <84998222+Nekidev@users.noreply.github.com>
2022-12-04 02:58:15 -0300

    Added documentation for /image/{id}

---
+++ pages/api/image.js
@@ -0,0 +1,28 @@
+export default function handler(req, res) {
+    res.status(200).json({ 
+        images: [
+            {
+                'id': 'xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                'url': 'https://discordapp.com/assets/xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.png',
+                'dimens': {
+                    'width': 1024,
+                    'height': 1024,
+                },
+                'artist': {
+                    'name': 'xxxxxxx',
+                    'url': 'https://example.com/xxxxxxx'
+                },
+                'source': {
+                    'name': 'xxxxxxx',
+                    'url': 'https://example.com/xxxxxxx'
+                },
+                'nsfw': false,
+                'categories': [
+                    'xxxxxxx',
+                    'xxxxxxx',
+                ],
+                'createdAt': '2021-01-01T00:00:00.000Z',
+            }
+        ]
+    })
+}
