543168d10dd3b3e2670a837bc52ed2d31e7f6fcc
Neki <84998222+Nekidev@users.noreply.github.com>
2022-12-04 16:56:54 -0300

    Added about page

--- theme.config.jsx
+++ theme.config.jsx
@@ -57,12 +57,6 @@ export default {
         )
     },
     sidebar: {
-        titleComponent: ({ title, type }) => {
-            if (type === 'separator') {
-                return <span className="cursor-default">{title}</span>
-            }
-            return <>{title}</>
-        },
         defaultMenuCollapseLevel: 0
     },
 }
