8b18d871ccc648007c9b288687b5c85e0132cb30
Neki <84998222+Nekidev@users.noreply.github.com>
2022-12-05 19:32:15 -0300

    Added changes

--- src/pages/api/image.js
+++ src/pages/api/image.js
@@ -1,48 +1,70 @@
-import { PrismaClient } from "@prisma/client";
-
-function isNumeric(str) {
-    if (typeof str != "string") return false;
-    return (
-        !isNaN(str) &&
-        !isNaN(parseInt(str))
-    );
-}
+import { Prisma, PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
-    const { count = "1", categories = "" } = req.query;
+    var { limit = "1", categories = "" } = req.query;
 
-    if (!(/^[0-9]+$/).test(count)) {
+    if (
+        !/^[0-9]+$/.test(limit) ||
+        parseInt(limit) < 1 ||
+        parseInt(limit) > 25
+    ) {
         res.status(400).json({
-            error: "Invalid count",
+            code: 400,
+            message: "Invalid value for `limit` parameter. Expected a number between 1 and 25.",
+            success: false,
         });
         return;
     }
 
-    // const prisma = new PrismaClient();
-    // const images = await prisma.$queryRaw`SELECT * FROM "Images" WHERE categories @> ('{}') ORDER BY RANDOM() LIMIT ${count}`;
+    const prisma = new PrismaClient();
+
+    categories = categories.split(',');
+    const images = await prisma.$queryRaw`SELECT * FROM "Images" WHERE categories @> ARRAY[${Prisma.join(categories)}] ORDER BY RANDOM() LIMIT ${parseInt(limit)}`;
+
+    var jsonImages = [];
+
+    for (const image of images) {
+        var file = await prisma.objects.findUnique({
+            where: {
+                id: image.file,
+            }
+        })
+
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/sign/nekos-api/${file.name}`, {
+            method: "post",
+            mode: "cors",
+            headers: {
+                "apikey": process.env.NEXT_PUBLIC_SUPABASE_KEY,
+                "Content-Type": "application/json",
+            },
+            data: JSON.stringify({
+                expiresIn: 60 * 60
+            })
+        })
+        const json = await response.json();
+        const signedUrl = json;
+
+        jsonImages.push({
+            id: image.id,
+            url: signedUrl,
+            dimens: {
+                height: image.height,
+                width: image.width,
+            },
+            artist: image.artist,
+            source: {
+                name: image.source_name,
+                url: image.source_url,
+            },
+            nsfw: image.nsfw,
+            categories: image.categories,
+            createdAt: image.created_at,
+        });
+    }
 
     res.status(200).json({
-        images: [
-            {
-                id: "xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-                url: "https://discordapp.com/assets/xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.png",
-                dimens: {
-                    width: 1024,
-                    height: 1024,
-                },
-                artist: {
-                    name: "xxxxxxx",
-                    url: "https://example.com/xxxxxxx",
-                },
-                source: {
-                    name: "xxxxxxx",
-                    url: "https://example.com/xxxxxxx",
-                },
-                nsfw: false,
-                categories: ["xxxxxxx", "xxxxxxx"],
-                createdAt: "2021-01-01T00:00:00.000Z",
-            },
-        ],
+        data: jsonImages,
+        success: true,
     });
 
     prisma.$disconnect();
--- src/pages/api/image/[imageId].js
+++ src/pages/api/image/[imageId].js
@@ -1,26 +1,45 @@
-export default function handler(req, res) {
-    res.status(200).json({ 
-        image: {
-            'id': 'xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-            'url': 'https://discordapp.com/assets/xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.png',
-            'dimens': {
-                'width': 1024,
-                'height': 1024,
-            },
-            'artist': {
-                'name': 'xxxxxxx',
-                'url': 'https://example.com/xxxxxxx'
-            },
-            'source': {
-                'name': 'xxxxxxx',
-                'url': 'https://example.com/xxxxxxx'
-            },
-            'nsfw': false,
-            'categories': [
-                'xxxxxxx',
-                'xxxxxxx',
-            ],
-            'createdAt': '2021-01-01T00:00:00.000Z',
+import { PrismaClient } from "@prisma/client";
+
+import supabase from "../../../utils/supabase/client";
+
+export default async function handler(req, res) {
+    const { imageId } = req.query;
+
+    if (!/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi.test(imageId)) {
+        res.status(400).json({
+            code: 400,
+            message: "Invalid value for `imageId` parameter. Expected a UUID.",
+            success: false,
+        });
+    }
+
+    const prisma = new PrismaClient();
+
+    const image = await prisma.images.findUnique({
+        where: {
+            id: imageId
         }
-    })
+    });
+
+    res.status(200).json({ 
+        data: {
+            'id': image.id,
+            'url': image.url,
+            'dimens': {
+                'width': image.width,
+                'height': image.height,
+            },
+            'artist': image.artist,
+            'source': {
+                'name': image.source_name,
+                'url': image.source_url
+            },
+            'nsfw': image.nsfw,
+            'categories': image.categories,
+            'createdAt': image.created_at,
+        },
+        success: true
+    });
+
+    prisma.$disconnect();
 }
