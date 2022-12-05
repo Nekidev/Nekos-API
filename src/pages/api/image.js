cc2bc9b26716c994129290a1dbaa1c0311e0dd17
Neki <84998222+Nekidev@users.noreply.github.com>
2022-12-04 23:31:30 -0300

    Initialized prisma schema

--- src/pages/api/image.js
+++ src/pages/api/image.js
@@ -1,28 +1,49 @@
-export default function handler(req, res) {
-    res.status(200).json({ 
+import { PrismaClient } from "@prisma/client";
+
+function isNumeric(str) {
+    if (typeof str != "string") return false;
+    return (
+        !isNaN(str) &&
+        !isNaN(parseInt(str))
+    );
+}
+
+export default async function handler(req, res) {
+    const { count = "1", categories = "" } = req.query;
+
+    if (!(/^[0-9]+$/).test(count)) {
+        res.status(400).json({
+            error: "Invalid count",
+        });
+        return;
+    }
+
+    // const prisma = new PrismaClient();
+    // const images = await prisma.$queryRaw`SELECT * FROM "Images" WHERE categories @> ('{}') ORDER BY RANDOM() LIMIT ${count}`;
+
+    res.status(200).json({
         images: [
             {
-                'id': 'xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-                'url': 'https://discordapp.com/assets/xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.png',
-                'dimens': {
-                    'width': 1024,
-                    'height': 1024,
+                id: "xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+                url: "https://discordapp.com/assets/xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.png",
+                dimens: {
+                    width: 1024,
+                    height: 1024,
                 },
-                'artist': {
-                    'name': 'xxxxxxx',
-                    'url': 'https://example.com/xxxxxxx'
+                artist: {
+                    name: "xxxxxxx",
+                    url: "https://example.com/xxxxxxx",
                 },
-                'source': {
-                    'name': 'xxxxxxx',
-                    'url': 'https://example.com/xxxxxxx'
+                source: {
+                    name: "xxxxxxx",
+                    url: "https://example.com/xxxxxxx",
                 },
-                'nsfw': false,
-                'categories': [
-                    'xxxxxxx',
-                    'xxxxxxx',
-                ],
-                'createdAt': '2021-01-01T00:00:00.000Z',
-            }
-        ]
-    })
+                nsfw: false,
+                categories: ["xxxxxxx", "xxxxxxx"],
+                createdAt: "2021-01-01T00:00:00.000Z",
+            },
+        ],
+    });
+
+    prisma.$disconnect();
 }
--- src/utils/db/index.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize(
-  `postgres://${process.env.POSTRGES_USERNAME}:${process.env.POSTRGES_PASSWORD}@${process.env.POSTRGES_HOST}:${process.env.POSTRGES_PORT}/${process.env.POSTRGES_DB_NAME}`
-);
-sequelize.authenticate();
-
-export default sequelize;
