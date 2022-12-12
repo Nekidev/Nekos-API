5781dca9d718243015dd2ad447366dfd47386347
Neki <84998222+Nekidev@users.noreply.github.com>
2022-12-12 16:51:13 -0300

    Added cors bypass to all api endpoints

--- src/utils/api/rate-limit.js
+++ src/utils/api/rate-limit.js
@@ -1,5 +1,6 @@
-import { Redis } from '@upstash/redis'
-import requestIp from 'request-ip'
+import { Redis } from "@upstash/redis";
+import requestIp from "request-ip";
+import NextCors from "nextjs-cors";
 
 const client = new Redis({
     url: process.env.UPSTASH_URL,
@@ -12,22 +13,26 @@ export default async function checkRateLimit(req, res) {
     const limit = 1;
     const ttl = 1;
 
-    const current = await client.get(key) || 0;
+    const current = (await client.get(key)) || 0;
 
-    await client.set(
-        key,
-        current + 1,
-        {
-            ex: ttl,
-            nx: true,
-        }
-    )
+    await client.set(key, current + 1, {
+        ex: ttl,
+        nx: true,
+    });
+
+    await NextCors(req, res, {
+        // Options
+        methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+        origin: "*",
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
 
     if (current >= limit) {
         res.setHeader("Retry-After", ttl);
         res.status(429).json({
             code: 429,
-            message: "You have exceeded the rate limit. Please try again later.",
+            message:
+                "You have exceeded the rate limit. Please try again later.",
             success: false,
         });
         return false;
