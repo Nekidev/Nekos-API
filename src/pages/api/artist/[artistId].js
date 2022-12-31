5b8107a91cf2302e244b61233bfd062b2f2bd55c
Neki <84998222+Nekidev@users.noreply.github.com>
2022-12-31 18:35:40 -0300

    Fixed syntax error

--- src/pages/api/artist/[artistId].js
+++ src/pages/api/artist/[artistId].js
@@ -1,16 +1,20 @@
-import { PrismaClient } from '@prisma/client';
-import { parseArtist } from '../../../utils/db/parsers';
-import checkRateLimit from '../../../utils/api/rate-limit';
+import { PrismaClient } from "@prisma/client";
+import { parseArtist } from "../../../utils/db/parsers";
+import checkRateLimit from "../../../utils/api/rate-limit";
 
 export default async function handler(req, res) {
     if (!(await checkRateLimit(req, res))) {
         // Rate limit exceeded
         return;
     }
-    
+
     const { artistId } = req.query;
 
-    if (!/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi.test(artistId)) {
+    if (
+        !/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi.test(
+            artistId
+        )
+    ) {
         res.status(400).json({
             code: 400,
             message: "Invalid value for `artistId` parameter. Expected a UUID.",
@@ -22,8 +26,8 @@ export default async function handler(req, res) {
 
     const artist = await prisma.artists.findUnique({
         where: {
-            id: artistId
-        }
+            id: artistId,
+        },
     });
 
     if (!artist) {
@@ -36,8 +40,8 @@ export default async function handler(req, res) {
 
     res.status(200).json({
         data: await parseArtist(artist, prisma),
-        success: true
-    );
+        success: true,
+    });
 
     prisma.$disconnect();
 }
