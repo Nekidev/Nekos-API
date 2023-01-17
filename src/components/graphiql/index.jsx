b106f997bb1cbdcc723c9f241fbe05e56a82cac2
Neki <84998222+Nekidev@users.noreply.github.com>
2023-01-17 15:18:23 -0300

    Removed code exporter plugin

--- src/components/graphiql/index.jsx
+++ src/components/graphiql/index.jsx
@@ -1,13 +1,12 @@
 import * as React from "react";
 import Link from "next/link";
+import Head from "next/head";
 import GraphiQL from "graphiql";
 import { useExplorerPlugin } from '@graphiql/plugin-explorer';
-import { useExporterPlugin } from '@graphiql/plugin-code-exporter';
 
 import styles from "./index.module.css";
 import "graphiql/graphiql.css";
 import '@graphiql/plugin-explorer/dist/style.css';
-import '@graphiql/plugin-code-exporter/dist/style.css';
 
 const defaultQuery = `# Welcome to Nekos API GraphQL API!
 #
@@ -27,64 +26,19 @@ query ExampleQuery($limit: Int!) {
 # - Ctrl/Cmd + Enter
 # - Click on the pink run button`;
 
-const removeQueryName = query =>
-    query.replace(
-        /^[^{(]+([{(])/,
-        (_match, openingCurlyBracketsOrParenthesis) =>
-            `query ${openingCurlyBracketsOrParenthesis}`,
-    );
-
-const getQuery = (arg, spaceCount) => {
-    const { operationDataList } = arg;
-    const { query } = operationDataList[0];
-    const anonymousQuery = removeQueryName(query);
-    return (
-        ` `.repeat(spaceCount) +
-        anonymousQuery.replace(/\n/g, `\n` + ` `.repeat(spaceCount))
-    );
-};
-
-const exampleSnippetOne = {
-    name: `Example One`,
-    language: `JavaScript`,
-    codeMirrorMode: `jsx`,
-    options: [],
-    generate: arg => `export const query = graphql\`
-${getQuery(arg, 2)}
-\`
-`,
-};
-
-const exampleSnippetTwo = {
-    name: `Example Two`,
-    language: `JavaScript`,
-    codeMirrorMode: `jsx`,
-    options: [],
-    generate: arg => `import { graphql } from 'graphql'
-
-export const query = graphql\`
-${getQuery(arg, 2)}
-\`
-`,
-};
-
-const snippets = [exampleSnippetOne, exampleSnippetTwo];
-
 export default function IDE({ }) {
     const [query, setQuery] = React.useState(defaultQuery);
     const explorerPlugin = useExplorerPlugin({
         query,
         onEdit: setQuery,
     });
-    const exporterPlugin = useExporterPlugin({
-        query,
-        snippets,
-        codeMirrorTheme: 'graphiql',
-    });
     return (
         <div style={{
             height: "100vh"
         }}>
+            <Head>
+                <title>GraphQL Playground - Nekos API</title>
+            </Head>
             <GraphiQL
                 fetcher={async (graphQLParams) => {
                     const data = await fetch("/api/graphql", {
@@ -102,7 +56,7 @@ export default function IDE({ }) {
                 variables={`{\n  "limit": 5\n}`}
                 query={query}
                 onEditQuery={setQuery}
-                plugins={[explorerPlugin, exporterPlugin]}
+                plugins={[explorerPlugin]}
             >
                 <GraphiQL.Logo>
                     <Link href="/" className="hover:opacity-90 flex flex-row items-center gap-2 text-sm">
