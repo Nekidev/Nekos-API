539c99f77bb4705c1cc5743165814b30d5316aae
Neki <84998222+Nekidev@users.noreply.github.com>
2023-01-20 01:25:06 -0300

    Created character listing endpoint

---
+++ src/pages/api/character.js
@@ -0,0 +1,55 @@
+import { PrismaClient } from "@prisma/client";
+import { parseCharacter } from '../../utils/db/parsers';
+import { middleware } from "../../utils/api";
+
+export default async function handler(req, res) {
+    const scopes = await middleware(req, res);
+
+    if (scopes === false) {
+        // A response has been sent by the middleware.
+        return;
+    }
+
+    var { limit = "10", offset = "0" } = req.query;
+
+    if (
+        !/^[0-9]+$/.test(limit) ||
+        parseInt(limit) < 1 ||
+        parseInt(limit) > 25
+    ) {
+        res.status(400).json({
+            code: 400,
+            message:
+                "Invalid value for `limit` parameter. Expected a number between 1 and 25.",
+            success: false,
+        });
+        return;
+    } else if (!/^[0-9]+$/.test(offset) || parseInt(offset) < 0) {
+        res.status(400).json({
+            code: 400,
+            message: "Invalid value for `offset` parameter. Expected a number greater than 0.",
+            success: false
+        })
+        return;
+    }
+
+    const prisma = new PrismaClient();
+
+    const characters = await prisma.characters.findMany({
+        take: parseInt(limit),
+        skip: parseInt(offset)
+    })
+
+    let data = []
+
+    for (const row of characters) {
+        data.push(await parseCharacter(row))
+    }
+
+    res.status(200).json({
+        data,
+        success: true,
+    });
+
+    prisma.$disconnect();
+}
