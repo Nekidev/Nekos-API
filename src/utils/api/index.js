a4388ad898990092e7407c72ab9510c9248463d3
Neki <84998222+Nekidev@users.noreply.github.com>
2023-01-22 01:16:32 -0300

    Added support for custom rate limits with access tokens

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -19,10 +19,41 @@ export async function middleware(
         return false;
     }
 
-    if (scopes.map((scope) => scope.toLowerCase()).includes("api:ratelimit:disabled")) {
+    var ratelimit_requests = 1;
+    var ratelimit_ttl = 1;
+    var ratelimit_timeout_limit = 10;
+
+    const rate_limit_scope = scopes.filter((value, index) => {
+        const exp = /^api:ratelimit:[0-9]+-[0-9]+-[0-9]+/;
+        return exp.test(value);
+    })[0];
+
+    if (rate_limit_scope !== undefined) {
+        const options = rate_limit_scope
+            .split(":")[2]
+            .split("-")
+            .map((value, index) => {
+                return parseInt(value);
+            });
+        ratelimit_requests = options[0];
+        ratelimit_ttl = options[1];
+        ratelimit_timeout_limit = options[2];
+    }
+
+    if (
+        scopes
+            .map((scope) => scope.toLowerCase())
+            .includes("api:ratelimit:disabled")
+    ) {
         return scopes;
     } else {
-        if (!(await checkRateLimit(req, res))) {
+        if (
+            !(await checkRateLimit(req, res, {
+                ttl: ratelimit_ttl,
+                limit: ratelimit_requests,
+                timeout_limit: ratelimit_timeout_limit,
+            }))
+        ) {
             return false;
         } else {
             return scopes;
