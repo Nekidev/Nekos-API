116f0e2fb8bec8d8a764f631a29de65827c81773
Neki <84998222+Nekidev@users.noreply.github.com>
2023-01-22 01:26:15 -0300

    Upgraded API version

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
             "/artist/:id/images",
         ],
         meta: {
-            version: "v1.5.2",
+            version: "v1.5.3",
             baseApiRoute: "/api"
         },
         success: true,
