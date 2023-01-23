627c8ec167dde001df6075e037aefab72ecdfe26
Neki <84998222+Nekidev@users.noreply.github.com>
2023-01-23 16:38:19 -0300

    Added low on resources banner

--- theme.config.jsx
+++ theme.config.jsx
@@ -46,10 +46,10 @@ export default {
         "link": "https://discord.gg/b9Fv3kEfXc"
     },
     banner: {
-        "key": "nekos-new-graphiql",
+        "key": "no-more-content-updates-until-upgrade",
         "text": (
-            <Link href="/graphiql" style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: ".5rem" }}>
-                Try the new GraphiQL playground!
+            <Link href="https://patreon.com/nekidev" style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: ".5rem" }}>
+                We're not currently adding more content to the API until upgrade because we're low on resources.
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ height: "1rem", width: "1rem" }}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                 </svg>
