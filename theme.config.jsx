a164de340b3a57317aba40726eb5d9ee5bc9881b
Neki <84998222+Nekidev@users.noreply.github.com>
2023-01-27 00:08:11 -0300

    Updated banner link

--- theme.config.jsx
+++ theme.config.jsx
@@ -49,7 +49,7 @@ export default {
     banner: {
         "key": "v2-in-development",
         "text": (
-            <Link href="https://patreon.com/nekidev" style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", gap: ".5rem" }}>
+            <Link href="https://github.com/Nekos-API/Nekos-API" style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", gap: ".5rem" }}>
                 <span className="font-mono">v2.0.0-alpha</span> is currently being developed. You can check the progress in the GitHub repository.
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ height: "1rem", width: "1rem" }}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
