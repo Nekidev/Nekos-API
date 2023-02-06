c3b4644967136a47c297aee39a8264214be7b08b
Neki <84998222+Nekidev@users.noreply.github.com>
2023-02-06 01:17:37 -0300

    Added ethical ads

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router"
 import { useConfig } from "nextra-theme-docs"
 
 import Link from "next/link"
+import Script from "next/script"
 
 export default {
     logo: () => {
@@ -47,10 +48,10 @@ export default {
         "link": "https://discord.gg/b9Fv3kEfXc"
     },
     banner: {
-        "key": "v2-in-development",
+        "key": "v2-almost-finished",
         "text": (
             <Link href="https://github.com/Nekos-API/Nekos-API" style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", gap: ".5rem" }}>
-                <span className="font-mono">v2.0.0-alpha</span> is currently being developed. You can check the progress in the GitHub repository.
+                ✨ <span className="font-mono">v2.0.0-alpha</span> is almost finished! You can check out the current progress at it's GitHub repository ✨
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ height: "1rem", width: "1rem" }}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                 </svg>
@@ -65,8 +66,79 @@ export default {
         )
     },
     sidebar: {
-        defaultMenuCollapseLevel: 0,
         toggleButton: true,
+    },
+    toc: {
+        extraContent: () => {
+            return (
+                <>
+                    <Script async={true} src="https://media.ethicalads.io/media/client/ethicalads.min.js" />
+                    <div id="ad-docs-toc-main">
+                        <div className="bordered" data-ea-publisher="nekosapicom" id="ad-docs-toc"></div>
+                    </div>
+                    <Script>{`
+                        const ad_container_main = document.getElementById("ad-docs-toc-main");
+                        const ad_container_alt = document.getElementById("ad-docs-toc-alt");
+                        const ad_element = document.getElementById("ad-docs-toc");
+
+                        function setAdTheme() {
+                            if (document.documentElement.className.includes("dark")) {
+                                ad_element.classList.add("dark");
+                            } else {
+                                ad_element.classList.remove("dark");
+                            }
+                        };
+
+                        function callback(mutationList, observer) {
+                            mutationList.forEach(function(mutation) {
+                                if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                                    setAdTheme();
+                                }
+                            });
+                        };
+                        
+                        const observer = new MutationObserver(callback);
+                        observer.observe(document.documentElement, { attributes: true });
+
+                        window.onresize = () => {
+                            if (window.innerWidth >= 1280 && ad_container_main.childElementCount == 0) {
+                                ad_container_main.appendChild(ad_element);
+                                ad_element.removeAttribute("data-ea-type");
+                                ad_element.querySelectorAll("img")[0].style.display = "block";
+                                ad_element.querySelectorAll(".ea-placement")[0].classList.add("ea-type-image")
+                                ad_element.querySelectorAll(".ea-placement")[0].classList.remove("ea-type-text")
+                                console.log("appended to main")
+                            } else if (window.innerWidth < 1280 && ad_container_alt.childElementCount == 0) {
+                                ad_container_alt.appendChild(ad_element);
+                                ad_element.setAttribute("data-ea-type", "text");
+                                ad_element.querySelectorAll("img")[0].style.display = "none";
+                                ad_element.querySelectorAll(".ea-placement")[0].classList.add("ea-type-text")
+                                ad_element.querySelectorAll(".ea-placement")[0].classList.remove("ea-type-image")
+                                console.log("appended to alt")
+                            }
+                        };
+
+                        window.onload = () => {
+                            setAdTheme();
+                            window.onresize();
+                        };
+                        window.onready = () => {
+                            setAdTheme();
+                            window.onresize();
+                        };
+                        setTimeout(window.onresize, 1000);
+                    `}</Script>
+                </>
+            )
+        }
+    },
+    main: (children) => {
+        return (
+            <div>
+                {children.children}
+                <div id="ad-docs-toc-alt"></div>
+            </div>
+        )
     },
     defaultShowCopyCode: true,
 }
